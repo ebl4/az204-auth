75d7b4e5d3fc2eb89b65d7352c5192363fcc59a0
Edson <Edson@DESKTOP-D0BM5A4>
2022-06-20 17:16:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+var _clientId = "APPLICATION_CLIENT_ID";
+var _tenantId = "DIRECTORY_TENANT_ID";
+
+var app = PublicClientApplicationBuilder
+    .Create(_clientId)
+    .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+    .WithRedirectUri("http://localhost")
+    .Build();
+
+string[] scopes = { "user.read" };
+AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+Console.WriteLine($"Token:\t{result.AccessToken}");
